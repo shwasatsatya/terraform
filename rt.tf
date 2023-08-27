da74556763f65d9bffce38cd3874f42ee6194848
shwasatsatya <134328406+shwasatsatya@users.noreply.github.com>
2023-08-27 13:42:27 +0530

    Update rt.tf

--- rt.tf
+++ rt.tf
@@ -1,10 +1,14 @@
 resource "aws_route_table" "my_rt" {
   vpc_id = aws_vpc.my_vpc.id
+route {
+cidr_block ="0.0.0.0/0"
+ gateway_id             = aws_internet_gateway.my_igw.id
+}
 }
 
-resource "aws_route" "my_route" {
-  route_table_id         = aws_route_table.my_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id            = aws_internet_gateway.my_igw.id
 
- }
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_rt.id
+}
+
