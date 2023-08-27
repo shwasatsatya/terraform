b513fd167dd003f3c59ea82e663bcb0883e688e4
shwasatsatya <134328406+shwasatsatya@users.noreply.github.com>
2023-08-27 13:08:08 +0530

    rtt.tf

--- rt.tf
+++ rt.tf
@@ -4,6 +4,7 @@ resource "aws_route_table" "my_rt" {
 
 resource "aws_route" "my_route" {
   route_table_id         = aws_route_table.my_rt.id
+  subnet_id               = aws_subnet.my_subnet.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id            = aws_internet_gateway.my_igw.id
 
